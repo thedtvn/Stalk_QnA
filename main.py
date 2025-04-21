beac0df27e802865085fbc8a010f224adc992600
thedtvn <duongtuan30306@gmail.com>
2025-04-21 20:01:49 +0700

    initial commit: add HTML, CSS, and JavaScript for QnA application with backend setup

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+from typing import AsyncIterator
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from aiohttp import web
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from pymongo.server_api import ServerApi
+
+mongo = web.AppKey("db_key", AsyncIOMotorDatabase)
+
+routes = web.RouteTableDef()
+
+
+@routes.view("/")
+async def index(_request: web.Request) -> web.FileResponse:
+    return web.FileResponse("./index.html")
+
+
+@routes.post("/api/ask")
+async def ask(request: web.Request) -> web.Response:
+    data = await request.json()
+    req_data = ["q", "d", "t"]
+    for i in req_data:
+        if i not in data:
+            return web.json_response({"error": "Fuck you scraper"}, status=400)
+    q = data["q"].strip()
+    d = data["d"]
+    t = data["t"]
+    if 10 < len(q) < 100:
+        return web.json_response({"error": "Fuck you scraper"}, status=400)
+
+    user_c = request.app[mongo]["users"]
+    question_c = request.app[mongo]["question"]
+
+    old_user = await user_c.find_one({"t": t})
+    if old_user is None:
+        await user_c.insert_one({"t": t, "ips": []})
+        old_user = await user_c.find_one({"t": t})
+
+    if request.remote not in old_user["ips"]:
+        await user_c.update_one({"t": t}, {"$push": {"ips": request.remote}})
+    await user_c.update_one({"t": d}, {"$set": {"device": d}})
+
+    await question_c.insert_one({
+        "t": t,
+        "question": q
+    })
+    return web.json_response({})
+
+
+routes.static('/static', "./static")
+
+
+async def db_context(app: web.Application) -> AsyncIterator[None]:
+    uri = os.environ.get("MONGO_URI")
+    client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))["STALK_QnA"]
+    app[mongo] = client
+    yield
+    client.close()
+
+@web.middleware
+async def proxy_middleware(request: web.Request, handler):
+    real_ip = request.headers.get("Cf-Connecting-Ip", request.remote) # Cloudflare header
+    try:
+        return await handler(request.clone(remote=real_ip))
+    except web.HTTPNotFound:
+        return web.HTTPFound("/")
+
+
+app = web.Application()
+app.middlewares.append(proxy_middleware)
+app.add_routes(routes)
+app.cleanup_ctx.append(db_context)
+web.run_app(app, port=int(os.getenv("PORT")))
---
+++ static/index.js
@@ -0,0 +1,128 @@
+
+let lastError = Date.now();
+
+function load_QnA_screan() {
+    const template = document.getElementById("message-template");
+    const messageContainer = document.getElementById("container");
+    messageContainer.innerHTML = "";
+    for (let node of template.content.childNodes) {
+        messageContainer.appendChild(node.cloneNode(true));
+    }
+    const btn = document.getElementById("ask");
+    btn.addEventListener("click", send);
+}
+
+function load_QnA_submited() {
+    const template = document.getElementById("ok-template");
+    const messageContainer = document.getElementById("container");
+    messageContainer.innerHTML = "";
+    for (let node of template.content.childNodes) {
+        messageContainer.appendChild(node.cloneNode(true));
+    }
+    const btn = document.getElementById("ask");
+    btn.addEventListener("click", load_QnA_screan);
+}
+
+function clearError() {
+    const err = document.getElementById("err");
+    if (!err) return;
+    if (Date.now() - lastError > 3000) {
+        err.style.display = "none";
+    } else {
+        setTimeout(clearError, 3000 - (Date.now() - lastError));
+    }
+}
+
+function sendError(message) {
+    const err = document.getElementById("err");
+    if (!err) return;
+    err.innerText = message;
+    err.style.display = "block";
+    lastError = Date.now();
+    setTimeout(clearError, 3000);
+}
+
+async function getCanvasFingerprint() {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.textBaseline = 'top';
+  ctx.font = "16px 'Arial'";
+  ctx.textBaseline = "alphabetic";
+  ctx.fillStyle = "#f60";
+  ctx.fillRect(125, 1, 62, 20);
+  ctx.fillStyle = "#069";
+  ctx.fillText("C@nv@s-FP!", 2, 15);
+  ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+  ctx.fillText("C@nv@s-FP!", 4, 17);
+
+  ctx.beginPath();
+  ctx.arc(50, 50, 25, 0, Math.PI * 2, true);
+  ctx.stroke();
+
+  const dataURL = canvas.toDataURL();
+
+  const msgUint8 = new TextEncoder().encode(dataURL);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  return hashHex;
+}
+
+function getSystemInfo() {
+  return navigator.userAgentData
+      .getHighEntropyValues([
+        "architecture",
+        "model",
+        "platform",
+        "platformVersion",
+        "fullVersionList"
+      ]);
+}
+
+async function send() {
+    const btn = document.getElementById("ask");
+    btn.disabled = true;
+    const question = document.getElementById("question").value.trim();
+    if (question == "") {
+        sendError("Vui lòng nhập câu hỏi");
+        btn.disabled = false;
+        return;
+    } else if (question.length > 100) {
+        sendError("Câu hỏi quá dài, vui lòng nhập câu hỏi ngắn hơn 100 ký tự");
+        document.getElementById("question").value = "";
+        btn.disabled = false;
+        return;
+    } else if (question.length < 10) {
+        sendError("Câu hỏi quá ngắn, vui lòng nhập câu hỏi dài hơn 10 ký tự");
+        document.getElementById("question").value = "";
+        btn.disabled = false;
+        return;
+    }
+
+    let body = {
+        t: await getCanvasFingerprint(),
+        q: question,
+        d: await getSystemInfo()
+    };
+
+    const response = await fetch("/api/ask", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    if (response.status != 200) {
+        sendError("Có lỗi xảy ra, vui lòng thử lại sau");
+        btn.disabled = false;
+        return;
+    }
+    btn.disabled = false;
+    load_QnA_submited();
+}
+
+(async () => {
+    load_QnA_screan();
+})()
